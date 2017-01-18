7eab1eb72707ca9596842d22216a1c80aa038043
Andrey Belikov <omgumail@yandex.ru>
2017-01-18 15:31:18 +0600

    add DataBaseManager class

--- src/DataBaseManager.java
+++ src/DataBaseManager.java
@@ -5,18 +5,29 @@ import java.sql.*;
  */
 public class DataBaseManager {
 
-    private static final String urlDB = "jdbc:mysql://localhost:3306/checkDB";
-    private static final String user = "root";
-    private static final String pass = "";
+    private static String urlDB = "jdbc:mysql://localhost:3306/dbast";
+    private static String user = "root";
+    private static String pass = "";
 
     private static Connection connect;
     private static Statement statement;
     private static ResultSet result;
 
-    DataBaseManager(String query){
+    DataBaseManager(String urlDB, String user, String pass){
+        this.urlDB = urlDB;
+        this.user = user;
+        this.pass = pass;
+    }
+    DataBaseManager(){
+
+    }
+
+    public ResultSet getResult(String query){
         try{
 
             connect = DriverManager.getConnection(urlDB, user, pass);
+
+            System.out.println(connect);
 
             statement = connect.createStatement();
 
@@ -25,10 +36,13 @@ public class DataBaseManager {
         }catch(Exception sqlEx){
             sqlEx.printStackTrace();
         }finally {
-            try{connect.close();}catch(Exception ex){}
-            try{statement.close();}catch(SQLException ex){}
-            try{result.close();}catch(SQLException ex){}
+            return result;
         }
     }
-
+    protected void finalize(){
+        try{connect.close();}catch(Exception ex){}
+        try{statement.close();}catch(Exception ex){}
+        try{result.close();}catch(Exception ex){}
+        System.out.println("OK");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.ResultSet;
+
 /**
  * Created by belikov.a on 17.01.2017.
  */
@@ -5,7 +7,23 @@ public class Main{
 
     public static void main(String[] args){
 
-        new DataBaseManager("");
+        DataBaseManager managerDB = new DataBaseManager();
+        //DataBaseManager managerDB = new DataBaseManager("jdbc:mysql://localhost:3306/next", "root", "");
+
+        ResultSet result = managerDB.getResult("SELECT * FROM `country`");
+
+        try {
+            while (result.next()) {
+                int id = result.getInt(1);
+                String name = result.getString(2);
+                String author = result.getString(3);
+                System.out.printf("id: %d, name: %s, author: %s %n", id, name, author);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        managerDB.finalize();
 
     }
 
